97ccc8f12e2802811b8c908bbee4b534cb7c422d
mdscr3 <160481957+mdscr3@users.noreply.github.com>
2024-03-04 11:21:37 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showArrow(ArrowNames.South)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+})
